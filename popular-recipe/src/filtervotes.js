b20a620a2e987db20654accda845d3d7af3b6714
Liana Lo <liana.lo@prezi.com>
2014-11-27 01:21:19 +0100

    solution

--- popular-recipe/src/filtervotes.js
+++ popular-recipe/src/filtervotes.js
@@ -1,6 +1,101 @@
+var fileSystem = require('fs');
+var path = require('path');
 
-function filtervotes() {
+var FILE_PATH = path.join(__dirname, '../testlog.txt');
 
+var finalAnswer = filtervotes(FILE_PATH);
+
+console.log(finalAnswer);
+function filtervotes(filePath) {
+	voteLogArray = parseStringIntoArray(readFile(filePath))
+	return calculateScoreForEachRecipe(voteLogArray);
 }
 
+function readFile(filePath) {
+    return fileSystem.readFileSync(filePath).toString();
+}
+
+function parseStringIntoArray(inputString) {
+    return inputString.split("\n");
+}
+
+
+function calculateScoreForEachRecipe(logArray) {
+	var scoreList = []; 
+	var logArrayLength = logArray.length;
+	for (var i = 0; i < logArrayLength; i++) {
+		var element = logArray[i];
+		if (!isNaN(element)) {
+			var logArraySlice = logArray.slice(i + 1, i + Number(element) + 1);
+			scoreList.push(countVotesForOneRecipe(logArraySlice));
+			// new recipe detected
+			// count score
+			// append score to list
+		} else {
+			// element is a name, so just move on
+			continue;
+		}
+	}
+	return scoreList;
+}
+
+function countVotesForOneRecipe(logArray) {
+	var set = {
+	    elements: [],
+	    add: function(item){
+	        if (this.has(item)){
+	            // console.log("Item is already in set.");
+	        } else {
+	            // console.log("Item added to set.");
+	            this.elements.push(item);
+	        }
+	    },
+	    has: function(item){
+	        for (var i = 0; i < this.elements.length; i++){
+	            if (item == this.elements[i]){
+	                return true;
+	            }
+	        }
+	        return false;
+	    }
+	};
+
+	var totalVotes = 0;
+	var logArrayLength = logArray.length;
+	for (var i = logArrayLength - 1; i >= 0; i--) {
+		var element = logArray[i];
+		var name = element.slice(0, element.length - 1);
+		var vote = element.slice(-1);
+
+		if (!set.has(name)) {
+			set.add(name);
+			totalVotes += parseVoteSymbol(vote);
+		}
+	}
+	return totalVotes;
+}
+
+function parseVoteSymbol(vote) {
+	if (vote == "+") {
+		return 1;
+	} else {
+		return -1;
+	}
+}
+
+// find number
+
+// function - go ahead in list by number and log the most recent vote of each person
+// use hash to keep track
+
+// loop
+
+// collect results into an array
+// return array
+
 module.exports.filtervotes = filtervotes;
+module.exports.readFile = readFile;
+module.exports.parseStringIntoArray = parseStringIntoArray;
+module.exports.parseVoteSymbol = parseVoteSymbol;
+module.exports.countVotesForOneRecipe = countVotesForOneRecipe;
+module.exports.calculateScoreForEachRecipe = calculateScoreForEachRecipe;
--- popular-recipe/test/test.js
+++ popular-recipe/test/test.js
@@ -1,7 +1,59 @@
-
-var assert = require("assert")
+var assert = require("assert");
 var filtervotes = require('../src/filtervotes.js').filtervotes;
+var readFile = require('../src/filtervotes.js').readFile;
+var parseStringIntoArray = require('../src/filtervotes.js').parseStringIntoArray;
+var parseVoteSymbol = require('../src/filtervotes.js').parseVoteSymbol;
+var countVotesForOneRecipe = require('../src/filtervotes.js').countVotesForOneRecipe;
+var calculateScoreForEachRecipe = require('../src/filtervotes.js').calculateScoreForEachRecipe;
 
-suite('filter recipe votes', function() {
-
+suite('readFile', function() {
+	test('read file into a string', function() {
+		var inputString ="4\ntilak +\ntilak +\ntilak -\ntilak +\n3\nratna +\nshashi -\nratna -\n3\nbhavani -\nbhavani +\nbhavani -";
+		assert.deepEqual(inputString, readFile('./testlog.txt'));
+	});
 });
+
+suite('parseStringIntoArray', function() {
+    test('parse string by newline separators', function() {
+        assert.deepEqual(['1','32','456'], parseStringIntoArray("1\n32\n456"));
+    });
+});
+
+suite('calculateScoreForEachRecipe', function() {
+
+	var scoreList = [ '4',
+					  'tilak +',
+					  'tilak +',
+					  'tilak -',
+					  'tilak +',
+					  '3',
+					  'ratna +',
+					  'shashi -',
+					  'ratna -',
+					  '3',
+					  'bhavani -',
+					  'bhavani +',
+					  'bhavani -' ]
+
+	test('array should only tally most recent vote', function(){
+		assert.deepEqual([1, -2, -1], calculateScoreForEachRecipe(scoreList));
+	});	
+})
+
+suite('countVotesForOneRecipe', function() {
+	test('array should only tally most recent vote', function(){
+		assert.equal(1, countVotesForOneRecipe(['tilak +', 'tilak +', 'tilak -', 'tilak +']));
+	});	
+	test('array should only tally most recent vote', function(){
+		assert.equal(-2, countVotesForOneRecipe(['tilak +', 'tilak +', 'tilak -', 'yoyo -']));
+	});	
+})
+
+suite('parseVoteSymbol', function() {
+	test('+', function(){
+		assert.equal(1, parseVoteSymbol("+"));
+	});	
+	test('-', function(){
+		assert.equal(-1, parseVoteSymbol("-"));
+	});	
+})

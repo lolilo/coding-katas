150db1c9fb9f28adffa83b4d1ec93813da8db21c
Liana Lo <liana.lo@prezi.com>
2014-11-27 10:43:28 +0100

    Point class and distance function implemented.

--- magneto/src/magneto.js
+++ magneto/src/magneto.js
@@ -1,9 +1,22 @@
 
 var magnetoEffect = { 
-	method1: function( param ) {
-	},
-	method2: function( param ) {
+	snap: function(magneticPoint, mousePoint, radius) {
+
+		var retrunedPoint = new Point(50, 50);
+
+		return retrunedPoint;
+	}, 
+	distance: function(point1, point2) {
+		return Math.sqrt(Math.pow(point1.x-point2.x, 2) + Math.pow(point1.y - point2.y, 2));
 	}
 };
 
+var Point = function(x, y){
+	this.x = x;
+	this.y = y;
+
+	return this;
+};
+
 module.exports.magnetoEffect = magnetoEffect;
+module.exports.Point = Point;
--- magneto/test/test.js
+++ magneto/test/test.js
@@ -3,11 +3,38 @@
 var assert = require('assert');
 
 var magnetoEffect = require('../src/magneto.js').magnetoEffect;
+var Point = require('../src/magneto.js').Point;
 
 suite('magnetoEffect', function() {
 
-  test( 'test', function() {
-    
+  test('one magnetic point; mouse clicks close enough to attach to it', function() {
+  		var magneticPoint = new Point(50, 50);
+  		var mousePoint = new Point(49, 50);
+  		var effectRadius = 5;
+
+  		var expectedResult = new Point(50, 50);
+  		assert.deepEqual(expectedResult, magnetoEffect.snap(magneticPoint, mousePoint, effectRadius));
+  });
+
+  test('distance', function(){
+		var point1 = new Point(0, 0);
+  		var point2 = new Point(1, 1);
+
+  		var result = magnetoEffect.distance(point1, point2);
+  		assert.equal(Math.sqrt(2), result);
+
   });
 
 });
+
+suite('Point Class', function () {
+	test('Point class has x and y attributes.', function(){
+		var point = new Point(10, 20);
+		assert.equal(point.x, 10);
+		assert.equal(point.y, 20);
+
+		var point2 = new Point(20, 30);
+		assert.equal(point2.x, 20);
+		assert.equal(point2.y, 30);
+	});
+})
